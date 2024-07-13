bc95a3be0abbf01bafa3812c22449359ee7e1473
ochumenno66 <ochumenno66@yandex.ru>
2024-07-13 15:59:53 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function showMessage() {
+  console.log("Я учу JavaScript!");
+}
+showMessage();
+
+
+
+const images = document.querySelectorAll(".img");
+let currentImageIndex = 0;
+
+function showImage(index) {
+  images.forEach((img, idx) => {
+    img.classList.remove("active");
+    if (idx === index) {
+      img.classList.add("active");
+    }
+  });
+}
+
+document.querySelector(".back").addEventListener("click", () => {
+  currentImageIndex =
+    currentImageIndex > 0 ? currentImageIndex - 1 : images.length - 1;
+  showImage(currentImageIndex);
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  currentImageIndex =
+    currentImageIndex < images.length - 1 ? currentImageIndex + 1 : 0;
+  showImage(currentImageIndex);
+});
