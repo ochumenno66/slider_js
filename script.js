517f29c3b0d909f16006e19c4c950dde2e1f8f93
ochumenno66 <ochumenno66@yandex.ru>
2024-07-13 23:52:27 +0300

    commit

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-const images = document.querySelectorAll(".img");
-let currentImageIndex = 0;
-
-function showImage(index) {
-  images.forEach((img, idx) => {
-    img.classList.remove("active");
-    if (idx === index) {
-      img.classList.add("active");
-    }
-  });
+function learnJavaScript() {
+  console.log("Я учу JavaScript!");
 }
+learnJavaScript();
 
-document.querySelector(".back").addEventListener("click", () => {
-  currentImageIndex =
-    currentImageIndex > 0 ? currentImageIndex - 1 : images.length - 1;
-  showImage(currentImageIndex);
-});
-
-document.querySelector(".next").addEventListener("click", () => {
-  currentImageIndex =
-    currentImageIndex < images.length - 1 ? currentImageIndex + 1 : 0;
-  showImage(currentImageIndex);
+const images = document.querySelectorAll("img");
+const buttons = document.querySelectorAll("button");
+let currentImage = 0;
+images[1].style.display = "none";
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    images[currentImage].style.display = "none";
+    currentImage = (currentImage + 1) % 2;
+    images[currentImage].style.display = "block";
+  });
 });
